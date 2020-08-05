20e2fcb35bfacceea5b43bf6f52ba9580569e4cf
TaraqB <68851196+TaraqB@users.noreply.github.com>
2020-08-05 17:15:13 +0530

    Update uid-gen-test.cpp

--- uid-gen-test.cpp
+++ uid-gen-test.cpp
@@ -9,8 +9,7 @@ TEST(UID, UID_is_generated_within_length_limit) {
 }
 TEST(UID, UID_is_string_has_5_dots) {
   std::string uid = uidgen();
-  std::count(uid, &uid + uid.length() + n, '.')
-  ASSERT_EQ(std::count(uid, &uid + uid.length() + n, '.'),5);
+  ASSERT_EQ(std::count(uid, &uid + uid.length(), '.'),5);
 }
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
